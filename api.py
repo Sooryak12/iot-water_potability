4697b3ac49bd0b28ac742dd4eba06cbc8c401643
Soorya Prakash <sooryaprakash84@gmail.com>
2021-11-29 00:42:36 +0530

    commented local deploy

--- api.py
+++ api.py
@@ -42,7 +42,7 @@ def deploy(values : str):
 
 
 
-if __name__=="__main__":
-   uvicorn.run(app, host='127.0.0.1', port=4000, debug=False)
+#if __name__=="__main__":
+#   uvicorn.run(app, host='127.0.0.1', port=4000, debug=False)
     
     
